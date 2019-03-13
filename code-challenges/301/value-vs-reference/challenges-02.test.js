a442d16e86492156eef9e9ff99b89d39ecc8ee55
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-12 17:50:27 -0700

    appendTheEnd function complete

--- code-challenges/301/value-vs-reference/challenges-02.test.js
+++ code-challenges/301/value-vs-reference/challenges-02.test.js
@@ -9,8 +9,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
-  // Solution code here...
-  return str;
+  return str + ' The end.';
 }
 
 /* ------------------------------------------------------------------------------------------------
