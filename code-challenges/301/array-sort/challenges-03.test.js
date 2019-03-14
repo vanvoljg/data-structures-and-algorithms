dba5d3e92de2c054c395fce8789009dc6f1521b2
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-13 21:19:07 -0700

    alphabetize function complete

--- code-challenges/301/array-sort/challenges-03.test.js
+++ code-challenges/301/array-sort/challenges-03.test.js
@@ -23,6 +23,7 @@ For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 
 const alphabetize = (arr) => {
   // Solution code here...
+  return arr.sort();
 };
 
 /* ------------------------------------------------------------------------------------------------
