edf67e4365c345e7bd8e7871055dd587c98e1ddc
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-17 20:05:49 -0700

    stepActions function complete

--- code-challenges/301/string-modification/challenges-05.test.js
+++ code-challenges/301/string-modification/challenges-05.test.js
@@ -126,6 +126,9 @@ Return a new array containing just the verbs. For example, ['Mix until evenly di
 const stepActions = (recipe) => {
   let result = [];
   // Solution code here...
+  recipe.steps.forEach ( (step) => {
+    result.push(step.split(' ').slice(0, 1)[0]); // split the string, extract first word, push to array
+  })
   return result;
 };
 
