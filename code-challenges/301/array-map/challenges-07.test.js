567a35fa1e49c61b924b0d658bc6033d4ab45c01
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-19 22:50:49 -0700

    mapTwoToThe function complete

--- code-challenges/301/array-map/challenges-07.test.js
+++ code-challenges/301/array-map/challenges-07.test.js
@@ -40,6 +40,7 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
+  return arr.map((num, idx) => Math.pow(2, num));
 };
 
 /* ------------------------------------------------------------------------------------------------
