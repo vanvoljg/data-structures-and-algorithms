a132a5d6adf775c9d9c5e622b7a3228302db94af
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-20 22:44:03 -0700

    notInFirstArray function complete

--- code-challenges/301/array-filter/challenges-08.test.js
+++ code-challenges/301/array-filter/challenges-08.test.js
@@ -42,6 +42,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
+  return arr.filter(element => !forbiddenValues.includes(element));
 };
 
 /* ------------------------------------------------------------------------------------------------
