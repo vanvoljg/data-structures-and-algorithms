331b111d0f4e2781d6aeb4e14b7449a4a74246a4
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-21 16:54:16 -0700

    evenOddNumericValues function complete

--- code-challenges/301/array-filter/challenges-08.test.js
+++ code-challenges/301/array-filter/challenges-08.test.js
@@ -168,6 +168,7 @@ For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 
 
 const evenOddNumericValues = (arr) => {
   // Solution code here...
+  return arr.filter(element => typeof(element) === 'number').map(number => number%2===0 ? 'even' : 'odd');
 };
 
 /* ------------------------------------------------------------------------------------------------
