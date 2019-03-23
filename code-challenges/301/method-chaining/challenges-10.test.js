4baeda9ab9abc59adb9cbbf74f7b215f05100400
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-23 16:48:00 -0700

    totalSum function complete

--- code-challenges/301/method-chaining/challenges-10.test.js
+++ code-challenges/301/method-chaining/challenges-10.test.js
@@ -32,6 +32,10 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 
 const totalSum = (input) => {
   // Solution code here...
+  return input.reduce((sum, subArray) => {
+    subArray.forEach(number => sum += number);
+    return sum;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
