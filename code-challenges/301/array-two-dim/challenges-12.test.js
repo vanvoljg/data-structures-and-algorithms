da27743dc40838a76488d9538f6530e28f02c2fa
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-03-26 21:02:05 -0700

    howManyTreats function complete

--- code-challenges/301/array-two-dim/challenges-12.test.js
+++ code-challenges/301/array-two-dim/challenges-12.test.js
@@ -77,6 +77,7 @@ const errands = [
 
 const howManyTreats = (arr) => {
   // Solution code here...
+  return arr[2].items[1].quantity;
 };
 
 /* ------------------------------------------------------------------------------------------------
