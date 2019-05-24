2fc5ce1b23064aa3f22d3ea2de0fd99407bd979b
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-05-24 13:29:31 -0700

    pulls node out to its own module

--- data-structures/linkedList/__tests__/linkedList.test.js
+++ data-structures/linkedList/__tests__/linkedList.test.js
@@ -10,38 +10,25 @@ Will return false when searching for a value in the linked list that does not ex
 Can properly return a collection of all the values that exist in the linked list
 */
 
-const {LinkedListSingle: LinkedList, NodeSingle: Node} = require('../linkedList.js');
-
-describe('Linked Lists are a collection of Nodes', () => {
-  describe('Nodes can be created on their own', () => {
-    it('With no parameters', () => {
-      let node = new Node;
-      expect(node).toBeInstanceOf(Node);
-      expect(node.value).toBeNull();
-      expect(node.next).toBeNull();
-    });
-  });
-});
-
+const LinkedList = require('../linkedList.js');
 
 describe('Singly linked lists', () => {
-  
   it('Can be created with no input', () => {
-    expect(new LinkedList).toBeInstanceOf(LinkedList);
+    expect(new LinkedList()).toBeInstanceOf(LinkedList);
   });
 
   it('Has a head property', () => {
-    expect(new LinkedList).toHaveProperty('head');
+    expect(new LinkedList()).toHaveProperty('head');
   });
 
   it('Has a tail property', () => {
-    expect(new LinkedList).toHaveProperty('tail');
+    expect(new LinkedList()).toHaveProperty('tail');
   });
 
-  it ('Has head and tail pointing to the same node when the list has only one node', () => {
+  it('Has head and tail pointing to the same node when the list has only one node', () => {
     let list = new LinkedList('value');
     expect(list.head).toEqual(list.tail);
-  })
+  });
 
   it('Can have values inserted at the head', () => {
     let list = new LinkedList('value1');
@@ -56,26 +43,29 @@ describe('Singly linked lists', () => {
   });
 
   it('Can have values inserted at the tail of an empty list', () => {
-    let list = new LinkedList;
+    let list = new LinkedList();
     list.append('value1');
     expect(list.tail.value).toStrictEqual('value1');
-  })
+  });
 
   it('Can properly insert multiple nodes into the linked list', () => {
     let list = new LinkedList('value1');
     list.insert('value2');
-    expect(list.head).toEqual({value:'value2',next:{value:'value1',next:null}});
+    expect(list.head).toEqual({
+      value: 'value2',
+      next: { value: 'value1', next: null },
+    });
   });
 
   it('Will return null when searching an empty list', () => {
-    let list = new LinkedList;
+    let list = new LinkedList();
     expect(list.includes('value')).toBeNull();
-  })
+  });
 
   it('Will return null when searching for no value', () => {
     let list = new LinkedList('value');
     expect(list.includes()).toBeNull();
-  })
+  });
 
   it('Will return true when finding a value that exists in the list', () => {
     let list = new LinkedList('value1');
@@ -90,7 +80,7 @@ describe('Singly linked lists', () => {
   });
 
   it('Can return a collection of the values that exist in the list', () => {
-    let list = new LinkedList;
+    let list = new LinkedList();
     expect(list.print()).toEqual([]);
     list.insert('value1');
     list.insert('value2');
@@ -100,12 +90,12 @@ describe('Singly linked lists', () => {
   it('Can insert before a specified value', () => {
     let list = new LinkedList('1');
     list.insertBefore('1', '2');
-    expect(list.head).toEqual({value:'2',next:{value:'1',next:null}});
+    expect(list.head).toEqual({ value: '2', next: { value: '1', next: null } });
   });
 
   it('can insert after a specified value', () => {
     let list = new LinkedList('1');
     list.insertAfter('1', '2');
-    expect(list.head).toEqual({value:'1',next:{value:'2',next:null}});
+    expect(list.head).toEqual({ value: '1', next: { value: '2', next: null } });
   });
 });
