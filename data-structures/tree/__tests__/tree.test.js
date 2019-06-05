702b6de4688d0bf049868bd5f3d983b5d19b2e00
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-05 16:39:36 -0700

    update tree and tests

--- data-structures/tree/__tests__/tree.test.js
+++ data-structures/tree/__tests__/tree.test.js
@@ -1,5 +1,49 @@
 'use strict';
 
-const BinaryTree = require('../tree.js');
+const { Node, BinaryTree, BinarySearchTree } = require('../tree.js');
 
+describe('BinaryTree class', () => {
+  it('can create an empty tree', () => {
+    let tree = new BinaryTree();
+    expect(tree.root).toBeNull();
+  });
 
+  it('can create a tree with a root node', () => {
+    let tree = new BinaryTree('value');
+    expect(tree.root).toBeDefined();
+    expect(tree.root.value).toBeDefined();
+    expect(tree.root.value).toEqual('value');
+    expect(tree.root.left).toBeNull();
+    expect(tree.root.right).toBeNull();
+  });
+
+  it('can add a left child and a right child to a root node', () => {
+    let tree = new BinarySearchTree(10);
+    tree.add(5);
+    tree.add(20);
+    expect(tree.root.value).toEqual(10);
+    expect(tree.root.left.value).toEqual(5);
+    expect(tree.root.right.value).toEqual(20);
+  });
+
+  it('can return a collection from preOrder traversal', () => {
+    let tree = new BinaryTree('top');
+    tree.root.left = new Node('left');
+    tree.root.right = new Node('right');
+    expect(tree.preOrder()).toEqual(['top', 'left', 'right']);
+  });
+
+  it('can return a collection from inOrder traversal', () => {
+    let tree = new BinaryTree('top');
+    tree.root.left = new Node('left');
+    tree.root.right = new Node('right');
+    expect(tree.inOrder()).toEqual(['left', 'top', 'right']);
+  });
+
+  it('can return a collection from postOrder traversal', () => {
+    let tree = new BinaryTree('top');
+    tree.root.left = new Node('left');
+    tree.root.right = new Node('right');
+    expect(tree.postOrder()).toEqual(['left', 'right', 'top']);
+  });
+});
--- data-structures/tree/tree.js
+++ data-structures/tree/tree.js
@@ -1,13 +1,13 @@
 'use strict';
 
-class Node {
+const {Queue} = require('../stacksAndQueues/lib/stacks-and-queues.js');
 
+class Node {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
-
 }
 
 class BinaryTree {
@@ -19,7 +19,7 @@ class BinaryTree {
     }
   }
 
-  preOrder(root, outputArray = []) {
+  preOrder(root = this.root, outputArray = []) {
     // base case; loose equal to cover undefined as well as null
     if (root == null) {
       return;
@@ -32,7 +32,7 @@ class BinaryTree {
     return outputArray;
   }
 
-  inOrder(root, outputArray = []) {
+  inOrder(root = this.root, outputArray = []) {
     // base case; loose equal to cover undefined as well as null
     if (root == null) {
       return;
@@ -43,9 +43,9 @@ class BinaryTree {
     this.inOrder(root.right, outputArray);
 
     return outputArray;
-}
+  }
 
-  postOrder(root, outputArray = []) {
+  postOrder(root = this.root, outputArray = []) {
     // base case; loose equal to cover undefined as well as null
     if (root == null) {
       return;
@@ -57,7 +57,65 @@ class BinaryTree {
 
     return outputArray;
   }
-
 }
 
-module.exports = BinaryTree;
+class BinarySearchTree extends BinaryTree {
+  constructor(value) {
+    super();
+    this.validateInput(value);
+    this.root = new Node(value);
+  }
+
+  add(value, root = this.root) {
+    this.validateInput(value);
+    if (root.value === value) {
+      throw new Error(`${value} already in the tree`);
+    }
+    if (value < root.value) {
+      if (root.left !== null) {
+        this.add(value, root.left);
+      } else {
+        root.left = new Node( value );
+        return value;
+      }
+    }
+    if (value > root.value) {
+      if (root.right !== null) {
+        this.add(value, root.right);
+      } else {
+        root.right = new Node( value );
+        return value;
+      }
+    }
+  }
+
+  contains(value) {
+    this.validateInput(value);
+    if (this.root === null) {
+      throw new Error('Cannot search an empty tree');
+    }
+    let queue = new Queue(this.root);
+    while(queue.peek()) {
+      let current = queue.dequeue();
+      if (current.value === value) {
+        return true;
+      }
+      current.left && queue.enqueue(current.left);
+      current.right && queue.enqueue(current.right);
+    }
+    return false;
+  }
+
+  validateInput(value) {
+    if (typeof value !== 'number') {
+      this.error();
+    }
+    return true;
+  }
+
+  error() {
+    throw new TypeError('value must be a number');
+  }
+}
+
+module.exports = { Node, BinaryTree, BinarySearchTree };
