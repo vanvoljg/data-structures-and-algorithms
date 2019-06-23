7fe72c1b4cca600b391178d12b64fcac8ac2ebb7
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-22 17:22:49 -0700

    updated LinkedList class

--- data-structures/hashtable/hashtable.js
+++ data-structures/hashtable/hashtable.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const {LinkedListSingle: LinkedList} = require('../linkedList/lib/linkedListSingle.js');
+const {NodeSingle: Node} = require('../linkedList/lib/nodeSingle.js');
+
+module.exports = class Hashtable {
+  constructor () {
+    this.prime = 727;
+    this.capacity = 1023;
+    this.internalStorage = new Array(this.capacity).fill(new LinkedList());
+  }
+
+  add (key, value) {
+  }
+
+  get (key) {
+  }
+
+  contains (key) {
+    return true;
+  }
+
+  hash (key) {
+  }
+
+};
