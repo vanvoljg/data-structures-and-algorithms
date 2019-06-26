201f2b49dffc451040d48a9079e560adcb83eaa0
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-26 15:42:35 -0700

    add type checking

--- code-challenges/401/leftJoin/left-join.js
+++ code-challenges/401/leftJoin/left-join.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const leftJoin = (leftMap, rightMap) => {
+  if (!(leftMap instanceof Map && rightMap instanceof Map)){
+    throw new TypeError('Inputs must be maps');
+  }
   const outputMap = new Map();
   for (const [leftKey, leftValue] of leftMap) {
     const rightValue = rightMap.get(leftKey) || null;
     outputMap.set(leftKey, [leftValue, rightValue]);
   }
+  return outputMap;
 };
 
 module.exports = leftJoin;
