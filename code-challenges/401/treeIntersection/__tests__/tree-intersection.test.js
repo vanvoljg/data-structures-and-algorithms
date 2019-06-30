48a2e2510b3ebba4ef6f612cd054aaf621d44022
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-30 00:22:19 -0700

    added code and test

--- code-challenges/401/treeIntersection/__tests__/tree-intersection.test.js
+++ code-challenges/401/treeIntersection/__tests__/tree-intersection.test.js
@@ -1,2 +1,75 @@
 'use strict';
 
+const {
+  treeIntersection,
+} = require('../tree-intersection.js');
+
+describe('treeIntersection function', () => {
+  const tree1 = {
+    root: {
+      value: 5,
+      left: {
+        value: 15,
+        left: {
+          value: 4,
+          left: null,
+          right: null,
+        },
+        right: {
+          value: 3,
+          left: null,
+          right: null,
+        },
+      },
+      right: {
+        value: 10,
+        left: {
+          value: 8,
+          left: null,
+          right: null,
+        },
+        right: {
+          value: 7,
+          left: null,
+          right: null,
+        },
+      },
+    },
+  };
+  const tree2 = {
+    root: {
+      value: 5,
+      left: {
+        value: 13,
+        left: {
+          value: 4,
+          left: null,
+          right: null,
+        },
+        right: {
+          value: 2,
+          left: null,
+          right: null,
+        },
+      },
+      right: {
+        value: 10,
+        left: {
+          value: 9,
+          left: null,
+          right: null,
+        },
+        right: {
+          value: 7,
+          left: null,
+          right: null,
+        },
+      },
+    },
+  };
+  const intersectionSet = new Set([5, 4, 10, 7]);
+  it('should find the intersection of two trees and return a Set', () => {
+    const outputSet = treeIntersection(tree1, tree2);
+    expect(outputSet).toEqual(intersectionSet);
+  });
+});
--- code-challenges/401/treeIntersection/tree-intersection.js
+++ code-challenges/401/treeIntersection/tree-intersection.js
@@ -1,2 +1,32 @@
 'use strict';
 
+function treeIntersection(tree1, tree2) {
+  const firstTreeSet = setFromTree(tree1.root);
+  return setOfCommonValues(tree2.root, firstTreeSet);
+}
+
+function setFromTree(root, outputSet = new Set()) {
+  if (root === null) {
+    return null;
+  }
+
+  outputSet.add(root.value);
+  setFromTree(root.left, outputSet);
+  setFromTree(root.right, outputSet);
+  return outputSet;
+}
+
+function setOfCommonValues(root, inputSet, outputSet = new Set()) {
+  if (root === null) {
+    return null;
+  }
+  if (inputSet.has(root.value)) {
+    outputSet.add(root.value);
+  }
+  setOfCommonValues(root.left, inputSet, outputSet);
+  setOfCommonValues(root.right, inputSet, outputSet);
+
+  return outputSet;
+}
+
+module.exports = { treeIntersection, setFromTree, setOfCommonValues };
